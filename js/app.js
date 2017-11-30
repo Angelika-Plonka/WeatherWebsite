283766828fd3eb805c0b9d7b22e69ee5c992eea2
Angelika Płonka <angelikaplonka89@gmail.com>
2017-11-30 12:56:18 +0100

    loaded default city

--- js/app.js
+++ js/app.js
@@ -1,32 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
+$(function() {
 
     let btnShow = $("#show");
     let btnCheck = $("#check");
-    let info = $("#info");
     let coordinates = $("#coordinates");
-    let weatherImg = $("img");
+    let weatherImg = $("img").first();
+    let cityImg = $("img").last();
     let humidity =$("#humidity");
     let pressure = $("#pressure");
     let wind = $("#wind");
 
+//========set Katowice as a default city========
     let latit = 50.25841;
     let long = 19.02754;
-    // let cityName = $("#cityName").val("Katowice");
-    // let cityName = $("#headerCity").val();
-    // console.log(cityName);
+    let cityName = $("#cityName").val("Katowice");
 
 
+    let apiCity = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=85f6fd69d859ba5ae84d901b8290ea31`;
+    let apiCoords = `https://api.openweathermap.org/data/2.5/weather?lat=${latit}&lon=${long}&APPID=85f6fd69d859ba5ae84d901b8290ea31`;
 
-    let apiUrlCity = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=85f6fd69d859ba5ae84d901b8290ea31`;
-
-    // let apiUrlCoords = `https://api.openweathermap.org/data/2.5/weather?lat=${latit}&lon=${long}&APPID=85f6fd69d859ba5ae84d901b8290ea31`;
 
 //========IMAGE OF A CITY========
     let getCityImage = () => {
 
         let cityName = $("#cityName").val();
         let imageUrl = `https://pixabay.com/api/?key=7194261-cb5353e414e4d782b7a39e798&q=${cityName}&image_type=photo`;
-
 
         $.ajax({
             url: imageUrl
@@ -39,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     let randomImage = (response) => {
-        $("#cityImage").html("<img src="+response.hits[Math.floor(Math.random() * response.hits.length) ].webformatURL+"/>");
+        cityImg.attr('src', `${response.hits[Math.floor(Math.random() * response.hits.length) ].webformatURL}`);
         $("#cityName").val('');
     }
 
@@ -47,12 +44,12 @@ document.addEventListener('DOMContentLoaded', () => {
     btnCheck.on('click', getCityImage);
 
 
-
+//========GET THE CURRENT LOCATION========
     let getLocation = () => {
         let geo = navigator.geolocation;
-        let info = $("#info");
+        let location = $("#location");
         if (geo) {
-            info.html = "Please wait a moment"
+            location.html = "Please wait a moment"
             btnCheck.disabled = true;
             console.log('Pobieranie lokalizacji');
             geo.getCurrentPosition(showPosition);
@@ -64,23 +61,21 @@ document.addEventListener('DOMContentLoaded', () => {
     let showPosition = position => {
         latit = position.coords.latitude;
         long = position.coords.longitude;
-        console.log(latit);
-        console.log(long);
         btnCheck.disabled = false;
-        console.log('Pobrano');
+        console.log('Pobrano lokalizację');
         getWeather();
     }
 
     btnCheck.on('click', getLocation);
 
+
+//========LOAD WEATHER========
     let getWeather = () => {
         let cityName = $("#cityName").val();
         let urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${latit}&lon=${long}&APPID=85f6fd69d859ba5ae84d901b8290ea31`;
 
         if (cityName) {
-            console.log('IN');
             urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=85f6fd69d859ba5ae84d901b8290ea31`;
-            console.log(urlWeather);
         }
         $.ajax({
             url: urlWeather
@@ -95,7 +90,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let loadWeather = (data) => {
         $("#headerCity").html(`${data.name}`);
-        coordinates.html(`(lat.: ${data.coord.lat.toFixed(2)}, lon.:${data.coord.lon.toFixed(2)})`);
+        coordinates.html(`(lat: ${data.coord.lat.toFixed(2)}, lon:${data.coord.lon.toFixed(2)})`);
 
         $("#description").html(`${data.weather[0].description}`);
         let srcIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
@@ -104,42 +99,13 @@ document.addEventListener('DOMContentLoaded', () => {
         humidity.html(`Humidity: ${data.main.humidity}%`);
         pressure.html(`Pressure: ${data.main.pressure}hPa`);
         wind.html(`Wind: ${data.wind.speed} km/h`);
-
     }
-
     btnShow.on('click', getWeather);
     btnCheck.on('click', getWeather);
 
+
+//========LAUNCHING========
     getWeather();
     getCityImage();
 
 });
-
-// let getCityImage = () => {
-//
-//     let cityName = $("#cityName").val();
-//
-//     let imageUrl = "https://pixabay.com/api/?key=7194261-cb5353e414e4d782b7a39e798&q=Katowice&image_type=photo";
-//
-//     if (cityName) {
-//         console.log("bbbbbbbbbbbb");
-//         let imageUrl = `https://pixabay.com/api/?key=7194261-cb5353e414e4d782b7a39e798&q=${cityName}&image_type=photo`;
-//     }
-//
-//
-//     $.ajax({
-//         url: imageUrl
-//     }).done(function (response) {
-//         randomImage(response)
-//
-//     }).fail(function (error) {
-//         console.log("Sorry but we don't have your city in our database")
-//     })
-// }
-//
-// let randomImage = (response) => {
-//     $("#cityImage").html("<img src="+response.hits[Math.floor(Math.random() * response.hits.length) ].webformatURL+"/>");
-//     $("#cityName").val('');
-// }
-//
-// btnShow.on('click', getCityImage);
--- js/out.js
+++ js/out.js
@@ -70,27 +70,24 @@
 "use strict";
 
 
-document.addEventListener('DOMContentLoaded', function () {
+$(function () {
 
     var btnShow = $("#show");
     var btnCheck = $("#check");
-    var info = $("#info");
     var coordinates = $("#coordinates");
-    var weatherImg = $("img");
+    var weatherImg = $("img").first();
+    var cityImg = $("img").last();
     var humidity = $("#humidity");
     var pressure = $("#pressure");
     var wind = $("#wind");
 
+    //========set Katowice as a default city========
     var latit = 50.25841;
     var long = 19.02754;
-    // let cityName = $("#cityName").val("Katowice");
-    // let cityName = $("#headerCity").val();
-    // console.log(cityName);
+    var cityName = $("#cityName").val("Katowice");
 
-
-    var apiUrlCity = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=85f6fd69d859ba5ae84d901b8290ea31";
-
-    // let apiUrlCoords = `https://api.openweathermap.org/data/2.5/weather?lat=${latit}&lon=${long}&APPID=85f6fd69d859ba5ae84d901b8290ea31`;
+    var apiCity = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=85f6fd69d859ba5ae84d901b8290ea31";
+    var apiCoords = "https://api.openweathermap.org/data/2.5/weather?lat=" + latit + "&lon=" + long + "&APPID=85f6fd69d859ba5ae84d901b8290ea31";
 
     //========IMAGE OF A CITY========
     var getCityImage = function getCityImage() {
@@ -108,18 +105,19 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     var randomImage = function randomImage(response) {
-        $("#cityImage").html("<img src=" + response.hits[Math.floor(Math.random() * response.hits.length)].webformatURL + "/>");
+        cityImg.attr('src', "" + response.hits[Math.floor(Math.random() * response.hits.length)].webformatURL);
         $("#cityName").val('');
     };
 
     btnShow.on('click', getCityImage);
     btnCheck.on('click', getCityImage);
 
+    //========GET THE CURRENT LOCATION========
     var getLocation = function getLocation() {
         var geo = navigator.geolocation;
-        var info = $("#info");
+        var location = $("#location");
         if (geo) {
-            info.html = "Please wait a moment";
+            location.html = "Please wait a moment";
             btnCheck.disabled = true;
             console.log('Pobieranie lokalizacji');
             geo.getCurrentPosition(showPosition);
@@ -131,23 +129,20 @@ document.addEventListener('DOMContentLoaded', function () {
     var showPosition = function showPosition(position) {
         latit = position.coords.latitude;
         long = position.coords.longitude;
-        console.log(latit);
-        console.log(long);
         btnCheck.disabled = false;
-        console.log('Pobrano');
+        console.log('Pobrano lokalizację');
         getWeather();
     };
 
     btnCheck.on('click', getLocation);
 
+    //========LOAD WEATHER========
     var getWeather = function getWeather() {
         var cityName = $("#cityName").val();
         var urlWeather = "https://api.openweathermap.org/data/2.5/weather?lat=" + latit + "&lon=" + long + "&APPID=85f6fd69d859ba5ae84d901b8290ea31";
 
         if (cityName) {
-            console.log('IN');
             urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=85f6fd69d859ba5ae84d901b8290ea31";
-            console.log(urlWeather);
         }
         $.ajax({
             url: urlWeather
@@ -161,7 +156,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var loadWeather = function loadWeather(data) {
         $("#headerCity").html("" + data.name);
-        coordinates.html("(lat.: " + data.coord.lat.toFixed(2) + ", lon.:" + data.coord.lon.toFixed(2) + ")");
+        coordinates.html("(lat: " + data.coord.lat.toFixed(2) + ", lon:" + data.coord.lon.toFixed(2) + ")");
 
         $("#description").html("" + data.weather[0].description);
         var srcIcon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
@@ -171,42 +166,13 @@ document.addEventListener('DOMContentLoaded', function () {
         pressure.html("Pressure: " + data.main.pressure + "hPa");
         wind.html("Wind: " + data.wind.speed + " km/h");
     };
-
     btnShow.on('click', getWeather);
     btnCheck.on('click', getWeather);
 
+    //========LAUNCHING========
     getWeather();
     getCityImage();
 });
-
-// let getCityImage = () => {
-//
-//     let cityName = $("#cityName").val();
-//
-//     let imageUrl = "https://pixabay.com/api/?key=7194261-cb5353e414e4d782b7a39e798&q=Katowice&image_type=photo";
-//
-//     if (cityName) {
-//         console.log("bbbbbbbbbbbb");
-//         let imageUrl = `https://pixabay.com/api/?key=7194261-cb5353e414e4d782b7a39e798&q=${cityName}&image_type=photo`;
-//     }
-//
-//
-//     $.ajax({
-//         url: imageUrl
-//     }).done(function (response) {
-//         randomImage(response)
-//
-//     }).fail(function (error) {
-//         console.log("Sorry but we don't have your city in our database")
-//     })
-// }
-//
-// let randomImage = (response) => {
-//     $("#cityImage").html("<img src="+response.hits[Math.floor(Math.random() * response.hits.length) ].webformatURL+"/>");
-//     $("#cityName").val('');
-// }
-//
-// btnShow.on('click', getCityImage);
 
 /***/ })
 /******/ ]);
