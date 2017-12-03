8e5a4521180fb1b99b9bac9324dd0acc6d55ef11
Angelika Płonka <angelikaplonka89@gmail.com>
2017-12-03 13:42:51 +0100

    changed a width of photo

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(function(){
 
 checkIfCityNameCorrect = cityName => {
     return cityName !== undefined && cityName.length !== 0;
-}
+};
 
 getWeatherDataAndFillView = (latit,long,cityName) => {
     let urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${latit}&lon=${long}&APPID=85f6fd69d859ba5ae84d901b8290ea31`;
@@ -27,7 +27,7 @@ getWeatherDataAndFillView = (latit,long,cityName) => {
     }).fail(function (error) {
         alert("Please enter city name without Polish characters");
     });
-}
+};
 
 displayWeatherData = data => {
     $("#headerCity").html(data.name);
@@ -38,20 +38,20 @@ displayWeatherData = data => {
     $("#pressure").html(`Pressure: ${data.main.pressure}hPa`);
     $("#wind").html(`Wind: ${data.wind.speed} km/h`);
     updateWeather(data);
-}
+};
 
 
 countTemp= temp => {
     let temper = (temp - 273).toFixed(0);
     return temper + "&#8451";
-}
+};
 
 generateCoordinatesHTML= data => `(lat: ${data.coord.lat.toFixed(2)}, lon:${data.coord.lon.toFixed(2)})`;
 
 updateWeather = data => {
     let srcIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     $("#weatherImg").attr('src', srcIcon);
-}
+};
 
 
 // Other functionalities
@@ -65,7 +65,7 @@ getLocation = () => {
     } else {
         $("#coordinates").html = "Geolocation isn't supported by your browser.";
     }
-}
+};
 
 getCityImage = cityName => {
     let imageUrl = `https://pixabay.com/api/?key=7194261-cb5353e414e4d782b7a39e798&q=${cityName}&image_type=photo`;
@@ -77,13 +77,13 @@ getCityImage = cityName => {
     }).fail(function (error) {
         console.log("Sorry but we don't have your city in our database");
     });
-}
+};
 
 getCityByName = () => {
     let cityName = $("#cityName").val();
     getCityImage(cityName);
     getLocation();
-}
+};
 
 let showPosition = position => {
     let cityName = $("#cityName").val();
@@ -96,4 +96,4 @@ let showPosition = position => {
 
 check = () =>{
     getLocation();
-}
+};
